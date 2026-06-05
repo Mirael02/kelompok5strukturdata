d8398e1134abe815f958afbfffa71a2533d4edfe
YUSRIL AHMADINEJA <YUSRIL AHMADINEJA@LAPTOP-D6E3USV7>
2026-06-05 14:25:27 +0700

    Menambahkan atribut dan fungsi pada source code Mahasiswa.java dan MataKuliah.java

--- src/main/java/praktikum/entitas/Mahasiswa.java
+++ src/main/java/praktikum/entitas/Mahasiswa.java
@@ -1,5 +1,19 @@
 package praktikum.entitas;
 
 public class Mahasiswa {
+    String NIM;
+    String Nama;
+    String Prodi;
     
+    public Mahasiswa(String nim, String nama, String prodi){
+        this.NIM = nim;
+        this.Nama = nama;
+        this.Prodi = prodi;
+    }
+    
+    public void tampilMahasiswa(){
+        System.out.println("NIM Mahasiswa: " + NIM);
+        System.out.println("Nama Mahasiswa: " + Nama);
+        System.out.println("Nama Prodi: " + Prodi);
+    }
 }
--- src/main/java/praktikum/entitas/MataKuliah.java
+++ src/main/java/praktikum/entitas/MataKuliah.java
@@ -1,5 +1,19 @@
 package praktikum.entitas;
 
 public class MataKuliah {
-
+    String kodeMK;
+    String namaMK;
+    int sks;
+    
+    public MataKuliah(String kodemk, String namamk, int sks){
+        this.kodeMK = kodemk;
+        this.namaMK = namamk;
+        this.sks = sks;
+    }
+    
+    public void tampilMataKuliah(){
+        System.out.println("Kode MataKuliah: " + kodeMK);
+        System.out.println("Nama MataKuliah: " + namaMK);
+        System.out.println("Jumlah SKS: " + sks);
+    }
 }
